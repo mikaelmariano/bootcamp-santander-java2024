240cb84441a99b19bd53acf21a75c50e700f168e
Mika Mariano <mikaelmariano@gmail.com>
2024-05-05 23:50:40 -0300

    Desafio Fluxo

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception {
+    // Construtor que aceita uma mensagem de erro como parâmetro
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
